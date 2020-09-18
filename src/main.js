41edf49b7d10cb2a57af461d4c2b822688e3d8b3
Wietse Wind <mail@wietse.com>
2020-09-18 13:09:41 +0200

    Move from credential JSON file to dotenv

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.less'
 import Store from './store'
+import config from 'dotenv'
 import { routes, beforeEnter } from './routes'
-import { domain, clientId, audience } from '../auth_config.json'
 import { Auth0Plugin } from './auth'
 
 Vue.config.productionTip = false
@@ -13,14 +13,20 @@ Vue.config.productionTip = false
 Vue.use(Antd)
 Vue.use(VueRouter)
 
+try {
+  config.parse()
+} catch (e) {
+  //
+}
+
 const router = new VueRouter({ mode: 'history', routes })
 router.beforeEach(beforeEnter)
 
 // Install the authentication plugin here
 Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
-  audience,
+  domain: process.env.AUTH0_DOMAIN || '',
+  clientId: process.env.AUTH0_CLIENTID || '',
+  audience: process.env.AUTH0_AUDIENCE || '',
   router,
   onRedirectCallback: appState => {
     router.push(
