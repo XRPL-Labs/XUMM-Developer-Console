b84d04e27c5feda8d7c804eb4a4f1f3ccd08f825
Wietse Wind <mail@wietse.com>
2024-01-11 04:14:10 +0100

    Change run script

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 # docker run -p 8080:8080 --rm -it --name node14 -v $(pwd):/home/node/app node:14 sh -c 'apt update ; apt install python2 git ; cd /home/node/app; npm install ; npm run serve'
 
-docker build -t node14-devconsole .
-docker run -p 8080:8080 --rm -it --name node14-devconsole node14-devconsole
+docker rmi node14-devconsole
+docker build --no-cache -t node14-devconsole .
+docker run -p 8080:8080 --rm -it --name node14-devconsole -v $(pwd):/home/node/app node14-devconsole sh -c 'cd /home/node/app; npm run serve'
 
 
